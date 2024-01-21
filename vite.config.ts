2a3316c852fe87b537e3705d29fcfb658dd023dd
Mimori256 <mimorimori256@gmail.com>
2024-01-21 14:47:54 +0900

    add deploy option

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,13 @@ import preact from "@preact/preset-vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "https://mimori256.github.io/Graduation-Checker/",
   plugins: [preact()],
   build: {
     rollupOptions: {
       plugins: [visualizer()],
     },
 
-    outDir: "build",
+    outDir: "dist",
   },
 });
